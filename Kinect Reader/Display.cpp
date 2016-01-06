e3093da42800b5ea789100f0c3f4ef70f00a588c
Kevin <kevin.blansit@gmail.com>
2016-01-05 22:59:13 -0800

    Edited display to work with the OpenGL commands; also removed testing code

--- Kinect Reader/Display.cpp
+++ Kinect Reader/Display.cpp
@@ -9,36 +9,6 @@ typedef steady_clock Clock;
 
 // unparameterized constructor
 Display::Display() {
-}
-
-// desctuctor
-Display::~Display() {
-
-}
-
-// public methods
-
-void Display::setBody(_skeleton body) {
-
-}
-
-// run method
-void Display::runDisplay() {
-    initSystem();
-    displayLoop();
-}
-
-// private methods
-
-// initializer private method
-int Display::initSystem() {
-    //TODO: add proper error checking
-
-    _x = INIT_X;
-    _y = INIT_Y;
-    _w = INIT_W;
-    _h = INIT_H;
-
     _currState = DisplayState::RUNNING;
 
     // initialize SDL
@@ -52,72 +22,59 @@ int Display::initSystem() {
         _scnWidth,
         _scnHeight,
         SDL_WINDOW_OPENGL);
-    if (_window == nullptr)
-        return 1;
+    if (_window == nullptr) {
+        cout << "Problem creating SDL window";
+        return;
+    }
+
+    SDL_GLContext glContext = SDL_GL_CreateContext(_window);
+    if (glContext == nullptr) {
+        cout << "Problem creating GL context";
+        return;
+    }
+
+    GLenum status = glewInit();
+    if (status != GLEW_OK) {
+        cout << "Problem initializing GLEW";
+        return;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
     // create renderer
     _rndr = SDL_CreateRenderer (
         _window,
         -1,
         SDL_RENDERER_ACCELERATED);
-    if (_rndr == nullptr)
-        return 2;
+    if (_rndr == nullptr) {
+        return;
+    }
 
-    // create texture
-    _txtr = SDL_CreateTexture(
-        _rndr,
-        SDL_PIXELFORMAT_ABGR8888,
-        SDL_TEXTUREACCESS_STREAMING,
-        _scnWidth,
-        _scnHeight);
-    if (_txtr == nullptr)
-        return 3;
-    
-    // set background colour and set renderer
-    SDL_SetRenderDrawColor(_rndr, 50, 100, 255, 0);
-    SDL_RenderClear(_rndr);
-
-    SDL_SetRenderDrawColor(_rndr, 0, 100, 0, 0);
-    _r.x = _x;
-    _r.y = _y;
-    _r.w= _w;
-    _r.h = _h;
-
-    SDL_RenderFillRect(_rndr, &_r);
-
-    // return 
-    SDL_RenderPresent(_rndr);
-
-    return 0;
-}
-void Display::displayLoop() {
-    while (_currState != DisplayState::OFF) {        
-        processInput();
-
-        // set square
-        _r.x = _x;
-        _r.y = _y;
-
-        // reset background colour and set renderer
-        SDL_SetRenderDrawColor(_rndr, 0, 0, 0, 0);
-        SDL_RenderClear(_rndr);
-
-        // draw square
-        SDL_SetRenderDrawColor(_rndr, 0, 100, 0, 0);
-        SDL_RenderFillRect(_rndr, &_r);
-        SDL_RenderPresent(_rndr);
-
-        SDL_RenderDrawLine(_rndr, INIT_X, INIT_Y, _x, _y);
-        SDL_RenderPresent(_rndr);
-
-        cout << "x: " << _x;
-        cout << "  ";
-        cout << "y: " << _y;
-        cout << "\r";
-
+    if (_vboID == 0) {
+        glGenBuffers(1, &_vboID);
     }
 }
 
+// desctuctor
+Display::~Display() {
+    SDL_GL_DeleteContext(_window);
+    SDL_DestroyWindow(_window);
+    SDL_Quit();
+}
+
+// public methods
+
+void Display::setBody(_skeleton body) {
+}
+
+// run method
+void Display::runDisplay() {
+    drawDisplay();
+}
+
+// private methods
+
+// process input
 void Display::processInput() {
     SDL_Event evnt;
 
@@ -127,21 +84,26 @@ void Display::processInput() {
         case SDL_QUIT:
             _currState = DisplayState::OFF;
             break;
-        case SDL_MOUSEMOTION:
-            _x = evnt.motion.x;
-            _y = evnt.motion.y;
-            validMove();
         }
     }
 }
 
+// draw display
 void Display::drawDisplay() {
+    while (_currState != DisplayState::OFF) {        
+        glClearColor(0.0f, 0.15f, 0.3f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-}
+        glBegin(GL_LINES);
 
-void Display::validMove() {
-    if (_x >= _scnWidth || _x <= 0)
-        _x = INIT_X;
-    if (_y >= _scnHeight || _y <= 0)
-        _y = INIT_Y;
+        glVertex2f(0.0, 0.0);
+        glVertex2f(0.1, 0.1);
+
+        glEnd();
+
+        // swap window
+        SDL_GL_SwapWindow(_window);
+
+        processInput();
+    }
 }
--- Kinect Reader/Display.h
+++ Kinect Reader/Display.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SDL\SDL.h>
 #include "Skeleton.h"
+#include "GL\glew.h"
 
 enum class DisplayState { RUNNING, OFF };
 
@@ -18,12 +19,8 @@ public:
 
 private:
     // methods
-    int initSystem();
-    void displayLoop();
     void processInput();
     void drawDisplay();
-
-    void validMove();
 
     // attributes
     SDL_Window* _window = nullptr;
@@ -32,21 +29,13 @@ private:
 
     SDL_Rect _r;
 
-    unsigned int _x;
-    unsigned int _y;
-    unsigned int _h;
-    unsigned int _w;
-
     DisplayState _currState;
         
+    GLuint _vboID = 0;
+
     // const attributes
     const unsigned int _scnWidth = 1024;
     const unsigned int _scnHeight = 768;
-
-    const unsigned int INIT_X = 50;
-    const unsigned int INIT_Y = 50;
-    const unsigned int INIT_H = 50;
-    const unsigned int INIT_W = 50;
 
     // structre of body coords for plotting
     // _body => _bodyPart => XYZ
